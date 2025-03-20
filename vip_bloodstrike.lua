c80024987f4048554f88f6312ccdc3c127cfca0a
REDCODZ-modz <r3d.c0d3.m0dz.@gmail.com>
2025-03-20 23:13:42 +0700

    vip_bloodstrike.lua

--- vip_bloodstrike.lua
+++ vip_bloodstrike.lua
@@ -1,4 +1,71 @@
--- Status fitur
+
+local url = "https://raw.githubusercontent.com/REDCODZ-modz/user.auth/refs/heads/main/uset.txt" 
+
+
+function checkLogin()
+    local input = gg.prompt({"Username:", "Password:"}, nil, {"text", "text"})
+    if not input then os.exit() end
+
+    local http = gg.makeRequest(url)
+    if not http or not http.content then
+        gg.alert("⚠ Gagal menghubungi server!")
+        os.exit()
+    end
+
+    local userData = http.content
+    local lines = {}
+
+
+    for line in userData:gmatch("[^\r\n]+") do
+        table.insert(lines, line)
+    end
+
+    local found = false
+    local expDate
+
+
+    for _, line in ipairs(lines) do
+        local user, pass, exp = line:match("([^:]+):([^:]+):([^:]+)")
+        if user == input[1] and pass == input[2] then
+            found = true
+            expDate = exp
+            break
+        end
+    end
+
+    if not found then
+        gg.alert("❌ Username atau Password salah! Script ditutup.")
+        os.exit()
+    end
+
+
+    checkExpiration(expDate, input[1])
+end
+
+
+function checkExpiration(expDate, username)
+    local year, month, day = expDate:match("(%d+)-(%d+)-(%d+)")
+    local expirationTime = os.time({year=tonumber(year), month=tonumber(month), day=tonumber(day), hour=23, min=59})
+
+    local now = os.time()
+    local remainingTime = expirationTime - now
+
+    if remainingTime <= 0 then
+        gg.alert("❌ Akun " .. username .. " sudah expired! Akses ditutup.")
+        os.exit()
+    else
+        local days = math.floor(remainingTime / 86400)
+        local hours = math.floor((remainingTime % 86400) / 3600)
+        local minutes = math.floor((remainingTime % 3600) / 60)
+
+
+        local message = string.format("✅ Login berhasil!\n\n⏳ Akun %s akan expired dalam:\n%d hari, %d jam, %d menit.", 
+                                      username, days, hours, minutes)
+
+        gg.alert(message)
+    end
+end
+    -- Status fitur
 status = {
     BYPASS = false,
     AIM_ASSIST_V2 = false,
